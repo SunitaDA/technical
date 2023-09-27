9eb3309b09f59d565257af8b953e446d0ed3eb08
SunitaDA <144669837+SunitaDA@users.noreply.github.com>
2023-09-27 11:13:52 +0530

    insert values.

---
+++ one/V1.1.16__employee2.sql
@@ -0,0 +1,2 @@
+INSERT INTO first.Employee (EmployeeID, FirstName, LastName, Email, PhoneNumber, HireDate, Department, Salary)
+VALUES (1, 'John', 'Doe', 'john.doe@example.com', '555-123-4567', '2023-01-15', 'Sales', 50000.00);
